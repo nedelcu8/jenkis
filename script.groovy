6a9e00e8eb74e877a2817ca441068d363470758a
Bogdan <macoveibogdan830@yahoo.com>
2023-05-31 15:11:00 +0300

    clean and build docker with versino incremement

--- script.groovy
+++ script.groovy
@@ -3,21 +3,24 @@ def incrementVersion(){
     sh   'mvn build-helper:parse-version versions:set \
           -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
           versions:commit'
-
-
+          def matcher = readFile('pom.xml') =~'<version>(.+)</version>'
+          def version = matcher[0][1] 
+          env.IMAGE_NAME = "$version-$BUILD_NUMBER"
 }
 
 def buildJar() {
     echo "building the application..."
+    sh 'ls -a'
+    sh 'mvn clean package'
     sh 'mvn package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nedelcu8/my-repo:jma-2.1 .'
+        sh "docker build -t nedelcu8/my-repo:$IMAGE_NAME ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nedelcu8/my-repo:jma-2.1'
+        sh "docker push nedelcu8/my-repo:$IMAGE_NAME"
     }
 } 
 
