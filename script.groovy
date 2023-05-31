7bdee50ad78991218ddf7cddabd917dc9157b730
Bogdan <78421973+nedelcu8@users.noreply.github.com>
2023-05-31 12:32:22 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,15 +1,11 @@
 def buildJar() {
     echo "building the application..."
-    sh 'mvn package'
+
 } 
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nedelcu8/my-repo:jma-2.1 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nedelcu8/my-repo:jma-2.1'
-    }
+    
 } 
 
 def deployApp() {
